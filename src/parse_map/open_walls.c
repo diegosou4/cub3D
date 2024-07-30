9859465d846bd3206aa0182b3010b8ea08c50ec3
diegosou4 <diegoaguia31@gmail.com>
2024-07-30 10:31:16 +0100

    (add/fix) function to load img xpm

---
+++ src/parse_map/open_walls.c
@@ -0,0 +1,17 @@
+#include "../../includes/cub3D.h"
+
+
+//  I need fix this function
+// to adpat to the new struct
+
+// t_img	load_img(int texture, t_game *game)
+// {
+// 	t_img	img;
+
+// 	img.relative_path = path;
+// 	img.img = mlx_xpm_file_to_image(varg->mlx, img.relative_path,
+// 			&img.img_width, &img.img_height);
+// 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+// 			&img.endian);
+// 	return (img);
+// }
