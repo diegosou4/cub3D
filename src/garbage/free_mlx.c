dd9ed31385e761b4299f596a3defd24ffe833b23
diegosou4 <diegoaguia31@gmail.com>
2024-08-14 20:42:45 +0100

    (add) fuction for help free mlx

---
+++ src/garbage/free_mlx.c
@@ -0,0 +1,21 @@
+#include "../../includes/cub3D.h"
+
+
+void free_walls(t_game *game, int texture)
+{
+    while(texture >= 0 && texture < 4)
+    {
+        if(game->wall[texture].texture.relative_path != NULL)
+            mlx_destroy_image(game->mlx, game->wall[texture].texture.img);
+        texture--;
+    }
+}
+
+void destroy_game(t_game *game)
+{
+    mlx_clear_window(game->mlx, game->win);
+	mlx_destroy_image(game->mlx, game->canva.img);
+	mlx_destroy_window(game->mlx,game->win);
+	mlx_destroy_display(game->mlx);
+	free(game->mlx);
+}
