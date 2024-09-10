220c29672897c983db5e3f5318758027f37b5801
diegosou4 <diegoaguia31@gmail.com>
2024-09-10 18:58:46 +0100

    (add) macros the game

---
+++ includes/macros.h
@@ -0,0 +1,55 @@
+#ifndef MACROS_H
+#define MACROS_H
+
+
+
+
+#define M_PI 3.14159265358979323846
+
+# define ESC 65307
+# define KEY_D 100
+# define KEY_A 97
+# define KEY_S 115
+# define KEY_D 100
+# define KEY_W 119
+# define L_AR 65361
+# define R_AR 65363
+
+
+# define FOV 60
+
+#define WIDTH 1280
+#define HEIGHT 720
+
+#define TAM_Y_P 32
+#define TAM_X_P 32
+#define TAM_P  8
+
+
+#define A_NORTH 270
+#define A_SOUTH 90
+#define A_WEST 180
+#define A_EAST 0
+
+enum	direction
+{
+	NORTH,
+	SOUTH,
+	EAST,
+	WEST
+};
+
+enum 	rbgtexture
+{
+	F,
+	C
+};
+
+enum option
+{
+	PARSE,
+	FINAL,
+	FLOOD
+};
+
+#endif
