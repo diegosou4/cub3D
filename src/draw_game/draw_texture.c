a7ac23f7f521485bda52b53c2833dae675ca8518
diegosou4 <diegoaguia31@gmail.com>
2024-11-09 16:04:09 +0000

    (update) refactoring

--- src/draw_game/draw_texture.c
+++ src/draw_game/draw_texture.c
@@ -22,7 +22,7 @@ void draw_walls(t_game *game, t_img *texture, double angle)
   }
 }
 
-void draw_walls_fade(t_game *game, t_img *texture, double angle, int pos)
+void draw_walls_fade(t_game *game, t_img *texture, double angle)
 {
 	double step;
 	double texPos;
@@ -63,38 +63,43 @@ void draw_walls_fade(t_game *game, t_img *texture, double angle, int pos)
 
 int return_pos(t_game *game)
 {
+	int texture_loc;
+	texture_loc = 0;
 	if(game->hit_door == true)
 		return 12;
 	if(game->player.ray.side == 1 && game->player.ray.rayDirY > 0)
-		return 1 + game->light_on;
+		texture_loc = 1 + game->light_on;
 	else if(game->player.ray.side == 0 && game->player.ray.rayDirX < 0)
-		return 4 + game->light_on;
+		texture_loc = 4 + game->light_on;
 	else if(game->player.ray.side == 0 && game->player.ray.rayDirX > 0)
-		return 7 + game->light_on;
+		texture_loc = 7 + game->light_on;
 	else if(game->player.ray.side == 1 && game->player.ray.rayDirY < 0)
-		return 10 + game->light_on;
-	return 0;
+		texture_loc = 10 + game->light_on;
+	texture_loc = check_world(game, texture_loc);
+	if (game->current_world == 1)
+			texture_loc -= game->light_on;
+	return texture_loc;
 }
 int check_world(t_game *game, int pos)
 {
 	if (game->inside_wall && !game->changed_world) 
 	{
     	game->changed_world = true;
-    if (game->current_world == 1) 
-	{
-        game->current_world = 0;
-        return (pos - game->current_world - game->light_on);
+    	if (game->current_world == 1) 
+		{
+			game->current_world = 0;
+			return (pos - game->current_world);
+		}
     } else if (game->current_world == 0) 
-	{
-
-        game->current_world = 1;
-        return (pos - game->current_world - game->light_on);
-    }
-	} else if (!game->inside_wall && game->changed_world) 
+		{
+			game->current_world = 1;
+			return (pos - game->current_world);
+    	}
+	else if (!game->inside_wall && game->changed_world) 
 	{
     	game->changed_world = false;
-	}
-	return (pos - game->current_world - game->light_on);
+	}	
+	return (pos - game->current_world);
 }
 
 
@@ -139,7 +144,7 @@ void	bu_timer(t_game *game)
 	current_time = time(NULL);
 	if (difftime(current_time, last_played_time) >= 10)
     {
-        system("paplay assets/music/BU.wav > /dev/null 2>&1 &");
+      //  system("paplay assets/music/BU.wav > /dev/null 2>&1 &");
         last_played_time = current_time;
     }
 }
@@ -209,7 +214,10 @@ void draw_texture(t_game *game, double angle)
 	double	wall_x;
 	double	rayx;
 	int		pos;
+	t_draw_func draw_func[2];
 
+	draw_func[0] = draw_walls_fade;
+	draw_func[1] = draw_walls;
 	draw_skyfloor(game, angle, game->player.ray.drawEnd, 0);
 	if (game->player.ray.side == 0)
 		wall_x = game->player.PosY + game->player.ray.perpWallDist * game->player.ray.rayDirY;
@@ -217,17 +225,10 @@ void draw_texture(t_game *game, double angle)
 		wall_x = game->player.PosX + game->player.ray.perpWallDist * game->player.ray.rayDirX;
 	wall_x -= floor(wall_x);
 	pos = return_pos(game);
-	if(pos != 12)
-	{
-		pos = check_world(game, pos);
-	}
 	rayx = (int)(wall_x * game->texture[pos].texture.img_width);
 	if (game->player.ray.side == 0 && game->player.ray.rayDirX > 0)
 		rayx = game->texture[pos].texture.img_width - rayx - 1;
 	if (game->player.ray.side == 1 && game->player.ray.rayDirY < 0)
 		rayx = game->texture[pos].texture.img_width - rayx - 1;
-	if(game->current_world == 0)
-		draw_walls_fade(game, &game->texture[pos].texture, rayx, 0);
-	else
-		draw_walls(game, &game->texture[pos].texture, rayx);
+	draw_func[game->current_world](game, &game->texture[pos].texture, rayx);
 }
