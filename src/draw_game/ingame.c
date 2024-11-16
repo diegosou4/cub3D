a8ef5338809dfaf79684c648ace4b5be94365dd4
diegosou4 <diegoaguia31@gmail.com>
2024-11-16 12:17:02 +0000

    (norminette)

--- src/draw_game/ingame.c
+++ src/draw_game/ingame.c
@@ -1,18 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ingame.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: diegmore <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/16 12:07:21 by diegmore          #+#    #+#             */
+/*   Updated: 2024/11/16 12:16:22 by diegmore         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../includes/cub3D.h"
 
-
-void ingame(t_game *game)
+static void	save_msc(t_game *game)
 {
-    const char *playCommand = "paplay assets/music/liminal.wav > /dev/null 2>&1 &";
-	start_map(game,0);
+	game->playCmd = ft_strdup("paplay assets/music/li.wav > /dev/null 2>&1 &");
+	game->playBur = ft_strdup("paplay assets/music/BU.wav > /dev/null 2>&1 &");
+}
+
+void	ingame(t_game *game)
+{
+	start_map(game, 0);
 	init_enemies(game);
 	draw_allray(game);
-
-	// system(playCommand);
+	save_msc(game);
+	if (game->playCmd != NULL)
+	{
+		system(game->playCmd);
+	}
 	mlx_hook(game->win, 2, (1L << 0), key_event, game);
 	mlx_hook(game->win, 3, (1L << 1), key_drop, game);
 	mlx_hook(game->win, 6, (1L << 6), mouse_track, game);
 	mlx_do_key_autorepeatoff(game->mlx);
-	mlx_loop_hook(game->mlx, mouse_monitor, game);	//consoante a mudança de posição ela executa um movimento
+	mlx_loop_hook(game->mlx, mouse_monitor, game);
 	mlx_loop(game->mlx);
 }
